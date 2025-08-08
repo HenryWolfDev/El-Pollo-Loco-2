5281838274973f30a0dd014208561e9848623eb4
Henry Wolf <HWolf.binz@gmail.com>
2025-08-09 00:21:05 +0200

    added BackgroundObjects

---
+++ models/BackgroundObject.js
@@ -0,0 +1,11 @@
+import { MoveableObject } from "./moveableObject.js";
+
+export class BackgroundObject extends MoveableObject {
+  width = 720;
+  height = 400;
+  constructor(imagePath, x, y) {
+    super().loadImage(imagePath);
+    this.x = x;
+    this.y = 480 - this.height;
+  }
+}
--- models/World.js
+++ models/World.js
@@ -2,11 +2,15 @@ import { IntervalHub } from "../js/IntervalHub.js";
 import { Character } from "./Character.js";
 import { Chicken } from "./Chicken.js";
 import { Cloud } from "./Cloud.js";
+import { BackgroundObject } from "./BackgroundObject.js";
 
 export class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Cloud(), new Cloud()];
+  backgroundObjects = [
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
+  ];
 
   ctx;
   canvas;
@@ -17,32 +21,22 @@ export class World {
   }
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.drawImage(
-      this.character.img,
-      this.character.x,
-      this.character.y,
-      this.character.height,
-      this.character.width
-    );
-    this.enemies.forEach((enemy) => {
-      this.ctx.drawImage(
-        enemy.img,
-        enemy.x,
-        enemy.y,
-        enemy.width,
-        enemy.height
-      );
-    });
-    this.clouds.forEach((cloud) => {
-      this.ctx.drawImage(
-        cloud.img,
-        cloud.x,
-        cloud.y,
-        cloud.width,
-        cloud.height
-      );
-    });
+
+    this.addObjectsToMap(this.clouds);
+    this.addObjectsToMap(this.backgroundObjects);
+    this.addToMap(this.character);
+    this.addObjectsToMap(this.enemies);
 
     requestAnimationFrame(() => this.draw());
+  }
+
+  addObjectsToMap(objects) {
+    objects.forEach((ob) => {
+      this.addToMap(ob);
+    });
+  }
+
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
 }
